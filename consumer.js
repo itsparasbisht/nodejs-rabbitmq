cf6bb28f4bf5fb1c123c9c1ae091d006194d3d33
itsparasbisht <iamparasbisht@gmail.com>
2023-07-11 20:32:18 +0530

    consumer file

--- consumer.js
+++ consumer.js
@@ -1,15 +1,18 @@
 const amqp = require("amqplib");
 require("dotenv").config();
 
-const msg = { number: process.argv[2] };
-console.log(process.argv);
 async function connect() {
   try {
     const connection = await amqp.connect(process.env.AMQP_HOST);
     const channel = await connection.createChannel();
     const result = await channel.assertQueue("jobs");
-    channel.sendToQueue("jobs", Buffer.from(JSON.stringify(msg)));
-    console.log("Job sent successfully");
+
+    channel.consume("jobs", (message) => {
+      const input = JSON.parse(message.content.toString());
+      console.log(input);
+    });
+
+    console.log("waiting for messages...");
   } catch (error) {
     console.log(error);
   }
